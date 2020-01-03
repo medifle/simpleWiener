265db8d3861e1d429e2cc3dbadc22903909c43a1
medifle <13296643+medifle@users.noreply.github.com>
2020-01-02 20:52:54 -0500

    add typing, rounds for simpleWiener; add a simple test

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
 # simpleWiener
-A simple implementation of Wiener's attack
+A simple and fast implementation of Wiener's attack against small RSA exponent `d`
 
 ## Requirement
 Python 3.8.0+
 
+## Usage
+```bash
+python3 simpleWiener.py e n
+```
+
 ## Example
 ```bash
-python3 simpleWiener.py
+python3 simpleWiener.py 9292162750094637473537 13029506445953503759481
+```
+output:
+```
+phi=13029506445724987531764
+k=363
+d=509
 ```
 
 ## License
---
+++ simpleWiener/simpleWiener.py
@@ -0,0 +1,89 @@
+import math
+from typing import Tuple, List
+
+
+##
+# recursive version, for the curious :)
+##
+# def cf_recur(n: int, d: int) -> List[int]:
+#     """Return the terms of the continued fraction when n is the numerator
+#     and d the divisor as a list
+#     ref: https://stackoverflow.com/questions/48094595/convert-fraction-to-continued-fraction"""
+#     if d == 0:
+#         return []  # Ok it is finished
+#     q = n // d  # compute the integer quotient
+#     r = n - q * d  # the rest
+#     return [q] + cf_recur(d, r)  # and recurse...
+
+
+# iterative version
+def cf(n: int, d: int) -> List[int]:
+    res = []
+    q, r = divmod(n, d)
+    while r != 0:
+        res = res + [q]
+        q, r = divmod(d, r)
+        d = (d - r) // q
+    return res + [q]
+
+
+def cf_to_convergent(cfe: list) -> Tuple[int, int]:
+    """
+    cfe: continued fraction expansion, a list
+    ref: https://www.cits.ruhr-uni-bochum.de/imperia/md/content/may/krypto2ss08/shortsecretexponents.pdf (6)
+    ref: https://github.com/orisano/owiener/blob/master/owiener.py
+    """
+    # owiener's version is slightly different from the pdf for q0, q1 to avoid extra if else
+    n1, n0 = 1, 0
+    d1, d0 = 0, 1
+
+    n, d = None, None
+    for q in cfe:
+        n = q * n1 + n0
+        d = q * d1 + d0
+        n0, d0 = n1, d1
+        n1, d1 = n, d
+    return n, d
+
+
+def is_square(n: int) -> bool:
+    return math.isqrt(n) ** 2 == n
+
+
+def check_phi(_k: int, _d: int, e: int, n: int, round: int = 10) -> bool:
+    for i in range(1, round + 1):
+        k = _k * i
+        d = _d * i
+        if k != 0 and (e * d - 1) % k == 0:
+            phi = (e * d - 1) // k
+            sum_pq = n - phi + 1
+            # we know p+q and pq, solve the quadratic equation using high school knowledge:
+            # given ax^2 - bx + c = 0, x = (-b ± sqrt(b^2 - 4ac))/2a
+            # we only need to know the solution x is an integer
+            # so we use is_square(...) to test if p and q are integer solution
+            # btw, p+q should be an even number if we assume none of p or q is 2
+            if sum_pq % 2 == 0 and is_square((sum_pq // 2) ** 2 - n):
+                print("\nphi={}\nk={}\nd={}".format(phi, k, d))
+                return True
+    return False
+
+
+def simple_wiener_attack(e: int, n: int) -> None:
+    cfe = cf(e, n)
+    for i in range(0, len(cfe)):
+        sub_cfe = cfe[:i + 1]
+        k, d = cf_to_convergent(sub_cfe)
+        if check_phi(k, d, e, n, 50):
+            return
+    print('Not Found')
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) > 1:
+        e = int(sys.argv[1])
+        n = int(sys.argv[2])
+        simple_wiener_attack(e, n)
+    else:
+        print("Usage: python3 simpleWiener.py e n")
